29c8b730fd83de06e724d2cb773601c99f294231
Jimmique <jimmique88@gmail.com>
2019-12-09 11:15:19 -0800

    Added Friend and AddFriend files to components, and added Navbar and styling.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return
-  <div className="App">
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <div className="navbar">
+          <NavLink to="/friendslist">Friends List</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
